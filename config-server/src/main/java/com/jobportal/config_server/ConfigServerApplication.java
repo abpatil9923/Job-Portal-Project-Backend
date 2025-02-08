df525459f4ae331677e625871d48bcafc459f4ed
Atul Patil <abpatil9923@gamil.com>
2025-02-08 23:49:49 +0530

    updated docker-build.yaml file

--- config-server/src/main/java/com/jobportal/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/jobportal/config_server/ConfigServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("config server application started...");
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
